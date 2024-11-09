77eb9a89f0d201a995e2e30adaac5df1d32d1742
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2024-11-09 22:18:16 +0200

    printtimuutokset bugibranchissä

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -7,10 +7,9 @@ from erotus import erotus
 logger("aloitetaan")
 
 x = int(input("luku 1: "))
-y = int(input("luku 2: "))print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
-print(f"{summa(x, y)}")
-print(f"{erotus(x, y)}")
+y = int(input("luku 2: "))
+print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # muutos bugikorjaus-branchissa
+print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # muutos bugikorjaus-branchissa
 
 logger("lopetetaan")
-print("goodbye!") # lisäys bugikorjaus-branchissa
+print("goodbye!")
