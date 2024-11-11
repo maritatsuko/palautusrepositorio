d938eed5d08acd64d7ea2e405eb8c5c7d43b2698
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2024-11-11 16:09:28 +0200

    t10

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,16 +5,17 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
     for player_dict in response:
         player = Player(player_dict)
-        players.append(player)
+        if player.nationality == "FIN":
+            players.append(player)
 
-    print("Oliot:")
+    print("Players from FIN:")
 
     for player in players:
         print(player)
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -9,4 +9,4 @@ class Player:
         self.id = dict['id']
     
     def __str__(self):
-        return f'{self.name:20} {self.nationality} {self.assists} {self.goals} {self.team}'
+        return f'{self.name:20} team {self.team} goals {self.goals} assists {self.assists}'
